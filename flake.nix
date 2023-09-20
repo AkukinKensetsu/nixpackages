9e35c01ece8215c9ec828c78d2274d947e6cf734
Vico <lewdovico@gnuweeb.org>
2023-09-21 02:44:29 +0900

    flake: use nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Ludovico's nixpkgs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
