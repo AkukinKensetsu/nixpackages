f9bac5f8df96d624c9870e3fef7c50ff4af49009
Ludovico Piero <lewdovico@gnuweeb.org>
2023-10-09 10:16:52 +0900

    flake: use `nixos-unstable` branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Ludovico's nixpkgs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-compat = {
       url = "github:edolstra/flake-compat";
